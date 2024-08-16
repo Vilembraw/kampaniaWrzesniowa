f96b067998c507d9ef10beeff2d1d4e721ce1688
Vilembraw <vilembraw@gmail.com>
2024-08-16 21:21:53 +0200

    lab 2 zad 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,24 @@ import static org.example.Segment.getProstopadly;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(150,150);
-        Point point2 = new Point(300,200);
-        Segment line = new Segment(point1,point2);
-        System.out.println(line.getLength());
-        System.out.println(line.toSvg());
+//        Point point1 = new Point(150,150);
+//        Point point2 = new Point(300,200);
+//        Segment line = new Segment(point1,point2);
+//        System.out.println(line.getLength());
+//        System.out.println(line.toSvg());
 
-        Segment[] prostLine = getProstopadly(point1,line);
-        System.out.println(prostLine[0].toSvg());
-        System.out.println(prostLine[1].toSvg());
+//        Segment[] prostLine = getProstopadly(point1,line);
+//        System.out.println(prostLine[0].toSvg());
+//        System.out.println(prostLine[1].toSvg());
 
         Point[] kwardratPoints = {new Point(100,100),new Point(200,100),new Point(200,200),new Point(100,200)} ;
         Polygon kwadratPolygon = new Polygon(kwardratPoints);
 
-        System.out.println(kwadratPolygon.toSvg());
+
+        SvgScene scene = new SvgScene();
+        scene.addPolygon(kwadratPolygon);
+        scene.save("scene.html");
+//        System.out.println(kwadratPolygon.toSvg());
     }
 
 }
---
+++ src/main/java/org/example/SvgScene.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SvgScene {
+    private ArrayList<Polygon> polygones = new ArrayList<>();
+
+    public void addPolygon(Polygon polygon){
+        polygones.add(polygon);
+    }
+    public void save(String path){
+        try {
+            FileWriter fileWriter = new FileWriter(path);
+            fileWriter.write("<HTML>");
+            fileWriter.write("<body>");
+            fileWriter.write("<svg height=\"200\" width=\"300\" xmlns=\"http://www.w3.org/2000/svg\">");
+            for(Polygon polygon : polygones)
+                fileWriter.write("\t" + polygon.toSvg() + "\n");
+            fileWriter.write("</svg>");
+            fileWriter.write("</body>");
+            fileWriter.write("</HTML>");
+            fileWriter.close();
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+            System.err.println("cant write" + path);
+        }
+    }
+}
