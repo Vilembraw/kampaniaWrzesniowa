312bd26c1f87b2f4f92580cc62de9b0661801b5e
Vilembraw <vilembraw@gmail.com>
2024-08-23 23:35:36 +0200

    lab 6 zad 1

---
+++ src/main/java/list/CustomList.java
@@ -0,0 +1,92 @@
+package list;
+
+
+import java.util.NoSuchElementException;
+
+public class CustomList<T> {
+    private class Node {
+        public Node next;
+        public T value;
+
+        public Node(T value) {
+            this.next = null;
+            this.value = value;
+        }
+    }
+
+    private Node head, tail;
+
+    public CustomList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void addLast(T value){
+        if(tail == null){
+            tail = new Node(value);
+            head = tail;
+        }
+        else{
+            tail.next = new Node(value);
+            tail = tail.next;
+        }
+    }
+
+
+    public T getLast(){
+        if(tail == null)
+            throw new NoSuchElementException("List is empty");
+        return tail.value;
+    }
+
+
+    public void addFirst(T value){
+        if(head == null){
+            head = new Node(value);
+            tail = head;
+        }else{
+            Node newHead = new Node(value);
+            newHead.next = head;
+            head = newHead;
+        }
+    }
+
+    public T getFirst(){
+        if(head == null){
+            throw new NoSuchElementException("List is empty");
+        }
+        return head.value;
+    }
+
+    public T removeFirst(){
+        if(head == null){
+            throw new NoSuchElementException("List is empty");
+        }
+        Node temp = head;
+        head = head.next;
+        return temp.value;
+    }
+
+    public T removeLast(){
+        if(tail == null){
+            throw new NoSuchElementException("List is empty");
+        }
+
+        if(head == tail){
+            Node temp = head;
+            head = tail = null;
+            return temp.value;
+        }else{
+            Node temp = head;
+            while(temp.next != tail) {
+                temp = temp.next;
+            }
+            tail = temp;
+            Node tempVal = temp.next;
+            temp.next = null;
+
+            return tempVal.value;
+        }
+
+    }
+}
---
+++ src/main/java/list/Main.java
@@ -0,0 +1,23 @@
+package list;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        {
+            CustomList<Integer> list = new CustomList<>();
+            list.addLast(1);
+            list.addLast(3);
+
+//            System.out.println(list.getFirst());
+//            System.out.println(list.getLast());
+
+            System.out.println(list.removeFirst());
+//            System.out.println(list.removeLast());
+            System.out.println(list.getFirst());
+            System.out.println(list.getLast());
+
+        }
+
+    }
+}
