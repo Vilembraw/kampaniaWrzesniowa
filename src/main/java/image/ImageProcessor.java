31d5a22c301079913c0d198b4ed575f3994037d9
Vilembraw <vilembraw@gmail.com>
2024-08-29 16:02:42 +0200

    lab 9 zad 2

--- src/main/java/image/ImageProcessor.java
+++ src/main/java/image/ImageProcessor.java
@@ -1,10 +1,13 @@
 package image;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+
+import static java.lang.Math.clamp;
 
 public class ImageProcessor {
 
@@ -20,5 +23,25 @@ public class ImageProcessor {
 
         File file = new File(path);
         ImageIO.write(bufferedImage, "jpg", file);
+    }
+
+    void setBrightness(int value){
+        for(int x = 0; x < bufferedImage.getWidth(); x++){
+            for(int y = 0; y < bufferedImage.getHeight(); y++){
+                int rgb = bufferedImage.getRGB(x, y);
+                int alpha = (rgb >> 24) & 0xFF;
+                if(alpha == 0){
+                    //transparent
+                }else{
+                Color color = new Color(bufferedImage.getRGB(x,y));
+
+                int red = clamp((color.getRed() + value),0,255);
+                int blue = clamp((color.getBlue() + value),0,255);
+                int green = clamp((color.getGreen() + value),0,255);
+                Color newColor = new Color(red, green, blue, alpha);
+                bufferedImage.setRGB(x, y, newColor.getRGB());
+                }
+            }
+        }
     }
 }
--- src/main/java/image/Main.java
+++ src/main/java/image/Main.java
@@ -7,6 +7,7 @@ public class Main {
         try {
             ImageProcessor imageProcessor = new ImageProcessor();
             imageProcessor.readImage("img.jpg");
+            imageProcessor.setBrightness(50);
             imageProcessor.writeImage("img1_old.jpg");
         } catch (IOException e) {
             throw new RuntimeException(e);
